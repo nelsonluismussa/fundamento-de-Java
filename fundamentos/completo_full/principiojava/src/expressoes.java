f7d7bb553be12d14d269e68d66ed59972aa9ba3b
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-07 22:06:12 +0200

    aula01

---
+++ fundamentos/completo_full/principiojava/src/expressoes.java
@@ -0,0 +1,20 @@
+public class expressoes {
+    public static void main(String[] args) {
+		// >  maior
+		// <  menor
+		// >=  maior ou igual
+		// <=  menor ou igual
+		// ==  igual
+		// !=   Deferente
+		
+		// Expressoes lógicas
+		// &&    : E
+		// ||    : ou
+		// !     : not
+		
+		System.out.println(!(4 >= 14) && !(90==1));
+		System.out.println(!(5!=1));
+		
+
+	}
+}
