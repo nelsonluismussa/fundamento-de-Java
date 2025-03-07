b4c7dfb07d7542f27eea09f971b656000ab6079f
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-07 17:45:59 +0200

    aula

--- fundamentos/completo_full/principiojava/src/Avariaveis01.java
+++ fundamentos/completo_full/principiojava/src/Avariaveis01.java
@@ -1,7 +1,13 @@
+
+import java.util.Scanner;
+
 public class Avariaveis01 {
 
     public static void main (String[] args){
 
+        Scanner input = new Scanner(System.in);
+
+        
     }
     
 }
