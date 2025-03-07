4ef37ea592bdc14f7ec256faa2249d29b1f4c432
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-07 18:45:43 +0200

    aula01

--- fundamentos/completo_full/principiojava/src/Avariaveis01.java
+++ fundamentos/completo_full/principiojava/src/Avariaveis01.java
@@ -1,12 +1,23 @@
 
-import java.util.Scanner;
 
 public class Avariaveis01 {
 
     public static void main (String[] args){
-
-        Scanner input = new Scanner(System.in);
-
+            // Variáveis e tipos básicos em java
+		// Declaracao de variáveis: <tipo> <nome> = valor;
+		// Tipos primitivos:
+			// -> Números inteiros: Byte, short, int, long
+			// -> Numeros com ponto flutante: float, double
+		   //  -> valor verdade: boolean
+		   //  -> Um caractere Unicode: char
+				String nomeCompleto = "Luis Mussa";
+				int idade = 10;
+				double salario = 1055.90;
+				boolean estadoCasado = true;
+				float altura = 1.70f;
+				char genero = 'M';
+		// Tipos de String: cadeia de caracteres (palavras, textos)
+		// Nomes de variáveis / padrão camel case
         
     }
     
