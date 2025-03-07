0fc02792cdbd32ec8cd1b1ada6e0f389b53674fd
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-07 18:56:44 +0200

    aul02

---
+++ fundamentos/completo_full/principiojava/src/entrdadedados.java
@@ -0,0 +1,22 @@
+public class entrdadedados {
+        
+	public static void main(String[] args) {
+		/*
+		 * Entrada de dados e saida de dados
+		 * Para escrever na tela um texto qualquer
+		 * 
+		 */
+		
+
+         	// Com quebra de linha ao final:
+		System.out.println("Bom noite!");
+        
+		// sem quebra de linha ao final:
+		System.out.print("Seja bem vindo!");
+		
+	
+	}
+    
+
+}
+
