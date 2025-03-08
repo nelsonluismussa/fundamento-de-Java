55137062fb0118e084df32ae9cde83731d05f392
Nelson L.Mussa <nelsonluismussa@gmail.com>
2025-03-08 10:53:25 +0200

    aula05

--- fundamentos/completo_full/principiojava/src/Repitacao.java
+++ fundamentos/completo_full/principiojava/src/Repitacao.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Repitacao {
+    	public static void main(String[] args) {
+
+		int num = 1;
+		int soma = 0;
+		Scanner input = new Scanner(System.in);
+		
+		while(num!=0) {
+			System.out.println("numero: ");
+			num = input.nextInt();
+			soma +=num;
+		}
+		System.out.println("Soma é: "+soma);
+
+	}
     
 }
